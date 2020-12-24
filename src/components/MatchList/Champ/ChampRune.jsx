8f56588a6a4dd41587f4568a72368d02c25d81ed
chicichich <pch6789@naver.com>
2020-12-24 23:01:48 +0900

    Update: ChampRune Component use redux state

--- src/components/MatchList/Champ/ChampRune.jsx
+++ src/components/MatchList/Champ/ChampRune.jsx
@@ -1,24 +1,30 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { useSelector } from 'react-redux';
 
 import ImgComponent from '../../common/ImgComponent';
 
-const RuneName = {
-  7200: 'Domination',
-  7201: 'Presision',
-  7202: 'Sorcery',
-  7203: 'Whimsy',
-  7204: 'Resolve',
-};
-
-//TODO: runeName에 없는 숫자가 들어올 경우 예외를 발생시켜야함
-
 export default function ChampRune({ firstRuneNum, secondRuneNum }) {
-  function makeRuneProps(runeNum) {
-    return {
-      src: `./img/runes/${runeNum}_${RuneName[runeNum]}.png`,
-      alt: RuneName[runeNum],
+  const runeJson = useSelector((state) => state.json.runesReforged);
+  if (!runeJson) return <div>loading</div>;
+  const runeData = {};
+  runeJson.forEach((pVal) => {
+    runeData[pVal.id] = {
+      src: `./img/${pVal.icon}`,
+      alt: pVal.name,
     };
+    pVal.slots.forEach(({ runes }) => {
+      runes.forEach(({ id, icon, name }) => {
+        runeData[id] = {
+          src: `./img/${icon}`,
+          alt: name,
+        };
+      });
+    });
+  });
+
+  function makeRuneProps(runeNum) {
+    return runeData[runeNum];
   }
   return (
     <div className="rune-box">
--- src/components/test/MatchList/Champ/ChampRune.test.js
+++ src/components/test/MatchList/Champ/ChampRune.test.js
@@ -1,5 +1,7 @@
 import React from 'react';
 
+import { useSelector } from 'react-redux';
+
 import Enzyme, { mount, shallow } from 'enzyme';
 import Adapter from '@wojtekmaj/enzyme-adapter-react-17';
 Enzyme.configure({ adapter: new Adapter() });
@@ -7,13 +9,19 @@ Enzyme.configure({ adapter: new Adapter() });
 import ChampRune from '../../../MatchList/Champ/ChampRune';
 import ImgComponent from '../../../common/ImgComponent';
 
+jest.mock('react-redux');
+
+const props = {
+  firstRuneNum: 1,
+  secondRuneNum: 1,
+};
+
 describe('ChampRune', () => {
-  const props = {
-    firstRuneNum: 7200,
-    secondRuneNum: 7200,
-  };
   let component;
   beforeEach(() => {
+    useSelector.mockImplementation(() => [
+      { id: 1, icon: 'test', name: 'test', slots: [] },
+    ]);
     component = shallow(<ChampRune {...props} />);
   });
   it('render without crashing', () => {
@@ -21,5 +29,16 @@ describe('ChampRune', () => {
   });
   it('has 2 img tags', () => {
     expect(component.find(ImgComponent)).toHaveLength(2);
+  });
+});
+
+describe('fail to get rune Json', () => {
+  let component;
+  beforeEach(() => {
+    useSelector.mockImplementation(() => false);
+    component = shallow(<ChampRune {...props} />);
+  });
+  it('rend text "loading"', () => {
+    expect(component.text()).toEqual('loading');
   });
 });
