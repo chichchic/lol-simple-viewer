042b16f5b8aa57f480d343d944c4eed8f984705f
chicichich <pch6789@naver.com>
2020-12-25 21:08:21 +0900

    Add: ItemBox, Item Component

---
+++ src/components/MatchList/Item/Item.jsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { useSelector } from 'react-redux';
+
+import ImgComponent from '../../common/ImgComponent';
+
+const itemImgUrl = 'http://ddragon.leagueoflegends.com/cdn/10.25.1/img/item/';
+
+export default function Item({ itemNum, className }) {
+  const itemJson = useSelector((state) => state.json.item.data);
+  if (!itemJson) return <div>Loading</div>;
+  if (itemNum < 0) {
+    return (
+      <div className={className}>
+        <ImgComponent src="./img/blank.png" alt="empty" className="empty" />
+      </div>
+    );
+  }
+  const itemInfo = itemJson[itemNum];
+  return (
+    <div className={className}>
+      <ImgComponent
+        src={itemImgUrl + itemInfo.image.full}
+        alt={itemInfo.name}
+        className="champPortrait"
+      />
+    </div>
+  );
+}
+
+Item.propTypes = {
+  itemNum: PropTypes.number.isRequired,
+};
---
+++ src/components/MatchList/Item/ItemBox.jsx
@@ -0,0 +1,40 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+import Item from './Item';
+
+const trinketList = [2052, 3340, 3363, 3364];
+
+function makeItemBox(itemArray) {
+  const componentArr = [];
+  let hasTrinket = false;
+  for (let i = 0; i < 7; i++) {
+    if (i >= itemArray.length) {
+      componentArr.push(-1);
+      continue;
+    }
+    if (trinketList.includes(itemArray[i])) {
+      hasTrinket = itemArray[i];
+    } else {
+      componentArr.push(itemArray[i]);
+    }
+  }
+  if (hasTrinket) {
+    componentArr.push(hasTrinket);
+  }
+  return componentArr;
+}
+
+export default function ItemBox({ itemArray }) {
+  return (
+    <article className="item-box">
+      {makeItemBox(itemArray).map((itemNum, idx) => (
+        <Item key={idx} itemNum={itemNum} />
+      ))}
+    </article>
+  );
+}
+
+ItemBox.prototype = {
+  itemArray: PropTypes.array.isRequired,
+};
