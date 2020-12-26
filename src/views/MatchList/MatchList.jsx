62ed2836d318c15e8edbd3bf4c1c4dc5788bc629
chicichich <pch6789@naver.com>
2020-12-26 23:19:38 +0900

    Update: rendering Rank Component

--- src/views/MatchList/MatchList.jsx
+++ src/views/MatchList/MatchList.jsx
@@ -1,13 +1,19 @@
 import React, { useEffect, useState } from 'react';
 import { useDispatch } from 'react-redux';
-import { useParams } from 'react-router-dom';
+import { useParams, useHistory } from 'react-router-dom';
 
 import InfoBoxList from '../../components/MatchList/InfoBoxList';
+import TextLink from '../../components/common/TextLink';
+import SearchBar from '../../components/common/SearchBar';
+import Profile from '../../components/MatchList/Profile';
+import Rank from '../../components/MatchList/Rank';
 
 import './MatchList.scss';
 
 import { getJson } from '../../store/action/json';
+
 const matchlistUrl = '/lol/match/v4/matchlists/by-account/';
+const gettingListNum = 5;
 
 async function getsummonerInfo(name) {
   const res = await fetch(`/lol/summoner/v4/summoners/by-name/${name}`, {
@@ -21,35 +27,73 @@ async function getsummonerInfo(name) {
   return summonerInfo;
 }
 
+async function getLeagueInfo(id) {
+  const res = await fetch(`/lol/league/v4/entries/by-summoner/${id}`, {
+    method: 'GET',
+    headers: {
+      'Accept-Charset': 'application/x-www-form-urlencoded; charset=UTF-8',
+      'X-Riot-Token': process.env.REACT_APP_LOL_API,
+    },
+  });
+  const leagueInfo = await res.json();
+  return leagueInfo;
+}
+
 export default function MatchList() {
-  const { name } = useParams();
   const dispatch = useDispatch();
+  const history = useHistory();
+  const { name } = useParams();
+
+  function searchsummoner(inputVal) {
+    //reset init data
+    history.push('/match-list/' + inputVal);
+  }
 
   const [gameIdList, setgameIdList] = useState([]);
   const [gameListIdx, setgameListIdx] = useState(0);
-  const [accountId, setAccountId] = useState(null);
+  const [account, setAccount] = useState(null);
+  const [iconId, setIconId] = useState(null);
+  const [level, setLevel] = useState(null);
+  const [leagueInfo, setLeagueInfo] = useState(null);
   function mergeGameIdList(newArr) {
     setgameIdList((oldArr) => [...oldArr, ...newArr]);
   }
   useEffect(() => {
     async function getJsonDatas() {
-      if (name === '') {
+      if (!name) {
         alert("Can't search empty string name");
         return;
       }
-      const { accountId } = await getsummonerInfo(name);
-      setAccountId(accountId);
+      const {
+        id,
+        profileIconId,
+        accountId,
+        summonerLevel,
+      } = await getsummonerInfo(name);
       await dispatch(getJson());
+      const league = await getLeagueInfo(id);
+      setAccount(accountId);
+      setIconId(profileIconId);
+      setLevel(summonerLevel);
+      setLeagueInfo(league);
     }
     getJsonDatas();
+    return () => {
+      setAccount(null);
+      setIconId(null);
+      setLevel(null);
+      setLeagueInfo(null);
+      setgameListIdx(0);
+      setgameIdList([]);
+    };
   }, [dispatch, name]);
 
   useEffect(() => {
     async function getmatchList(gameListIdx) {
       let url =
         matchlistUrl +
-        accountId +
-        `?beginIndex=${gameListIdx}&endIndex=${gameListIdx + 5}`;
+        account +
+        `?beginIndex=${gameListIdx}&endIndex=${gameListIdx + gettingListNum}`;
       const req = await fetch(url, {
         method: 'GET',
         headers: {
@@ -60,15 +104,37 @@ export default function MatchList() {
       const { matches } = await req.json();
       mergeGameIdList(matches);
     }
-    if (accountId) getmatchList(gameListIdx);
-  }, [gameListIdx, accountId]);
+    if (account) getmatchList(gameListIdx);
+  }, [gameListIdx, account]);
 
   return (
-    <section>
-      <InfoBoxList gameIdList={gameIdList} />
-      <button onClick={() => setgameListIdx((oldVal) => oldVal + 5)}>
-        bububuton
-      </button>
+    <section className="match-list">
+      <div className="top-nav">
+        <TextLink label="LoLSimpleViewer" url={`/`} fontSize="2rem" />
+        <SearchBar onClick={searchsummoner} />
+      </div>
+      <div className="content">
+        <div className="left-content">
+          {iconId && level && (
+            <Profile profileIconId={iconId} summonerLevel={level} />
+          )}
+          {leagueInfo && <Rank leagueInfo={leagueInfo} />}
+        </div>
+
+        <div className="right-content">
+          <div className="info-list">
+            <InfoBoxList gameIdList={gameIdList} />
+            <button
+              className="show-more-btn"
+              onClick={() =>
+                setgameListIdx((oldVal) => oldVal + gettingListNum)
+              }
+            >
+              SHOW MORE
+            </button>
+          </div>
+        </div>
+      </div>
     </section>
   );
 }
