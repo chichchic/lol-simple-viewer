6edfb69b95b6af3c600df3968892a0aeb808e992
chicichich <pch6789@naver.com>
2021-01-01 12:32:49 +0900

    Add: TimeList view

--- src/Routes.jsx
+++ src/Routes.jsx
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 import Main from './views/Main/Main';
 import MatchList from './views/MatchList/MatchList';
+import TimeLine from './views/TimeLine/TimeLine';
 
 class Routes extends React.Component {
   render() {
@@ -11,6 +12,7 @@ class Routes extends React.Component {
         <Switch>
           <Route exact path="/" component={Main} />
           <Route exact path="/match-list/:name" component={MatchList} />
+          <Route exact path="/time-line/:matchId" component={TimeLine} />
         </Switch>
       </Router>
     );
---
+++ src/views/TimeLine/TimeLine.jsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export default function TimeLine() {
+  return <section className="time-line">123</section>;
+}
