c51e1bf5cd20a3e456b49ee5884d2a0779febdca
chicichich <pch6789@naver.com>
2021-01-03 22:34:52 +0900

    Add: Board component

---
+++ src/components/TimeLine/Board.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+
+import BoardLine from './BoardLine';
+
+import './Board.scss';
+
+export default function Board({ eventLogs, participantChamps, curTime }) {
+  if (!eventLogs) return <div>loading</div>;
+  function showBoadLine() {
+    const eventLine = [];
+    for (const key in eventLogs) {
+      if (key > curTime) break;
+      eventLine.push(
+        <BoardLine
+          participantChamps={participantChamps}
+          event={eventLogs[key]}
+          timestamp={key}
+          key={key}
+        />,
+      );
+    }
+    return eventLine;
+  }
+  return <article class="board">{showBoadLine()}</article>;
+}
