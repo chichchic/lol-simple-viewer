fa0e78e040a40e62b9fd72dff2005f7d231b2231
chicichich <pch6789@naver.com>
2021-01-10 00:46:13 +0900

    Add: ComponentLoading.scss
    make align-center

--- src/components/common/ComponentLoading.jsx
+++ src/components/common/ComponentLoading.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import ReactLoading from 'react-loading';
+
+import './ComponentLoading.scss';
 export default function Loading() {
   return (
     <section className="component-loading">
