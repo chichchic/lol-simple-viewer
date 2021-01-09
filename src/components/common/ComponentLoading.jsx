73bdf9a35996fcacfbe199a0f31894dc0b634ccb
chicichich <pch6789@naver.com>
2021-01-10 00:41:51 +0900

    Rename: LoadingComponent.jsx

--- src/components/common/ComponentLoading.jsx
+++ src/components/common/ComponentLoading.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactLoading from 'react-loading';
 export default function Loading() {
   return (
-    <section className="loading">
+    <section className="component-loading">
       <ReactLoading type="bubbles" />
     </section>
   );
